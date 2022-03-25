9ab981d5f97e67f81e43378f8e8c8a866e45a1c2
Pavel2202 <paveltimenov22@gmail.com>
2022-03-25 21:48:16 +0200

    Add Regular Expressions - Lab

---
+++ C# Fundamentals/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Match_Full_Name
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(names);
+
+            foreach (var item in matches)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02._Match_Phone_Number
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"( ?\+359 2 \d{3} \d{4})|(\+ ?359-2-\d{3}-\d{4})";
+            var matches = Regex.Matches(Console.ReadLine(), pattern);
+            var listofnumbers = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                listofnumbers.Add(match.Value.Trim());
+            }
+
+            Console.WriteLine(string.Join(", ", listofnumbers));
+        }
+    }
+}
