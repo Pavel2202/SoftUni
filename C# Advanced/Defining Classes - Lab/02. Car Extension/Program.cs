c315be96e86c07441295d5ea2876147cff58751c
Pavel2202 <paveltimenov22@gmail.com>
2022-04-17 20:38:56 +0300

    Add DefiningClasses - Lab

---
+++ C# Advanced/Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
