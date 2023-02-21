9e75f441a69f159d97ec1b79afeaf259f98530d1
Pavel2202 <paveltimenov3@gmail.com>
2023-02-21 14:18:06 +0200

    Add Error Controller

--- JS Backend/JS Back-End Retake Exam - 14 Dec 2021/config/routes.js
+++ JS Backend/JS Back-End Retake Exam - 14 Dec 2021/config/routes.js
@@ -2,10 +2,12 @@ const homeController = require("../controllers/home");
 const authController = require("../controllers/auth");
 const userController = require("../controllers/user");
 const publicationController = require("../controllers/publication");
+const errorController = require("../controllers/error");
 
 module.exports = (app) => {
   app.use(homeController);
   app.use(authController);
   app.use(userController);
   app.use(publicationController);
+  app.use(errorController);
 };
