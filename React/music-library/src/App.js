2a4bba07bcb14fe19301bc526632658c0016b672
Pavel2202 <paveltimenov3@gmail.com>
2023-03-09 12:33:46 +0200

    Add Home page

--- React/music-library/src/App.js
+++ React/music-library/src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div id="container">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+
+      <footer>
+        <p>&copy;MusicLibrary</p>
+      </footer>
     </div>
   );
 }
